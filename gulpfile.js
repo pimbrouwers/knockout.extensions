d8c3935ee5055c1f3f196feb4237cd8d8b3ac41e
Pim Brouwers <mpbrouwers@gmail.com>
2016-07-11 21:34:26 -0400

    gulp workflow added

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+﻿/// <binding Clean='clean' />
+"use strict";
+
+var gulp = require("gulp"),    
+    uglify = require("gulp-uglify");
+
+gulp.task("minify:js", function(){
+	return gulp.src('./src/knockout-extensions.js')
+				.pipe(uglify())
+				.pipe(gulp.dest('./dist'));
+});
+
+gulp.task("default", ["minify:js"]);
